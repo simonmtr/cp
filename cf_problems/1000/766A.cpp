2da6f361dd5d1d7b1307b095031f1719e982cfc0
simonmtr <294simon@googlemail.com>
2025-04-28 19:34:56 +0200

    766A

---
+++ cf_problems/1000/766A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string a, b;
+    cin >> a >> b;
+    if (a==b) {
+        cout << -1;
+    } else {
+        cout << max(a.length(), b.length());
+    }
+}
