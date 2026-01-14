7d03ad285b7be21a33616aad08166acfab3622c6
simonmtr <294simon@googlemail.com>
2026-01-14 20:17:24 +0100

    LC107

---
+++ problems/leetcode/107.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = deque()
+        queue = deque([root])
+
+        while queue:
+            level = []
+            queuelen = len(queue)
+            for i in range(queuelen):
+                node = queue.popleft()
+                if node:
+                    level.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+            if level:
+                res.appendleft(level)
+        return list(res)
+
+
+
+
